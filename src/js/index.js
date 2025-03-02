1e39541a6bd855f3101a6c26ac831890e318d01d
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-03-02 23:14:29 +0700

    add popular this summer section

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,10 @@ var categoriesSwiper = new Swiper("#Categories", {
     slidesOffsetAfter: 20,
     slidesOffsetBefore: 20,
 });
+
+var startYourSwiper = new Swiper("#PopularSummerSlider", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    slidesOffsetAfter: 20,
+    slidesOffsetBefore: 20,
+});
